e28069798480d7e5827459a7c022d011c48450e2
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2021-11-15 15:14:57 -0800

    Initial Commit

---
+++ maze1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  maze1
+//
+//  Created by Abel Christy Anand on 11/15/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
